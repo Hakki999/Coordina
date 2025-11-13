51cb900b6c963f4c07dcc2b7fc7b426b5767e1bf
Hakki999 <josemiguelbar9@gmail.com>
2025-11-13 14:26:21 -0300

    v2 Adição de envio de mensagem por whatsgit push -m origin main

---
+++ js/WhatsAppSession/whatsAppRest.js
@@ -0,0 +1,49 @@
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+let clientReady = false;
+let client;
+
+function initializeWhatsApp() {
+    client = new Client({
+        authStrategy: new LocalAuth(),
+        puppeteer: {
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        }
+    });
+
+    client.on('qr', qr => {
+        console.log('📱 Escaneie o QR Code abaixo com o WhatsApp:');
+        qrcode.generate(qr, { small: true });
+    });
+
+    client.on('ready', () => {
+        console.log('✅ WhatsApp conectado com sucesso!');
+        clientReady = true;
+    });
+
+    client.initialize();
+}
+
+function sendMSG(number, mensagem) {
+    if (!clientReady) {
+        console.log('❌ WhatsApp não está pronto. Mensagem não enviada.');
+        return 
+    }
+
+    console.log(number);
+    
+
+    return client.sendMessage(`55${number}@c.us`, mensagem)
+        .then(() => console.log('📤 Mensagem enviada com sucesso!'))
+        .catch(err => {
+            console.error('❌ Erro ao enviar mensagem:', err);
+            throw err;
+        });
+}
+
+// Inicializa o cliente do WhatsApp
+initializeWhatsApp();
+
+module.exports = { sendMSG };
